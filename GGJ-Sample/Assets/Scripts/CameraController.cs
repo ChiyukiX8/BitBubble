d4366f0b2f570a2c905a27e82b2ea09918da1c7a
ChiyukiX8 <chiyukiX8@gmail.com>
2025-01-26 13:37:46 -0600

    Snaps to center on focus

--- GGJ-Sample/Assets/Scripts/CameraController.cs
+++ GGJ-Sample/Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,20 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float Speed = 5.0f;
+    public Vector3 startPos;
+
+    private void OnEnable(){
+        AppEvents.OnBubbleClick.OnTrigger += SnapToBubble;
+    }
+
+    private void OnDisable(){
+        AppEvents.OnBubbleClick.OnTrigger -= SnapToBubble;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = this.transform.position;
     }
 
     // Update is called once per frame
@@ -44,5 +54,9 @@ public class CameraController : MonoBehaviour
         {
             Camera.main.orthographicSize += Speed * Time.deltaTime;
         }
+    }
+
+    void SnapToBubble(Guid id){
+        this.transform.position = startPos;
     }
 }
