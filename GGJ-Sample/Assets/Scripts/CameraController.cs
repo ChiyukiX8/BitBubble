331afeb3f4b3c17a733e6fc3935e09c4d8635161
ChiyukiX8 <ChiyukiX8@gmail.com>
2025-01-26 09:46:27 -0600

    Inverting Camera Controls

--- GGJ-Sample/Assets/Scripts/CameraController.cs
+++ GGJ-Sample/Assets/Scripts/CameraController.cs
@@ -17,22 +17,22 @@ public class CameraController : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || (Input.mousePosition.y >= Screen.height && Input.mousePosition.y < Screen.height * 1.05))
         {
-            transform.position -= new Vector3(0, 1.0f, 0) * Speed * Time.deltaTime;
+            transform.position += new Vector3(0, 1.0f, 0) * Speed * Time.deltaTime;
         }
 
         if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || (Input.mousePosition.y <= Screen.height *(1 - 0.975) && Input.mousePosition.y > -1.25))
         {
-            transform.position += new Vector3(0, 1.0f, 0) * Speed * Time.deltaTime;
+            transform.position -= new Vector3(0, 1.0f, 0) * Speed * Time.deltaTime;
         }
 
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || (Input.mousePosition.x <= Screen.width *(1 - 0.975) && Input.mousePosition.x > -1.25))
         {
-            transform.position += new Vector3(1.0f, 0, 0) * Speed * Time.deltaTime;
+            transform.position -= new Vector3(1.0f, 0, 0) * Speed * Time.deltaTime;
         }
 
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || (Input.mousePosition.x >= Screen.width && Input.mousePosition.x < Screen.width * 1.05))
         {
-            transform.position -= new Vector3(1.0f, 0, 0) * Speed * Time.deltaTime;
+            transform.position += new Vector3(1.0f, 0, 0) * Speed * Time.deltaTime;
         }
     }
 }
