25f1941421dc38a18021f42a3c597d31fb9a32dc
ChiyukiX8 <ChiyukiX8@gmail.com>
2025-01-26 13:41:09 -0600

    Adding the focus feature for when player clicks on a bubble
    Ideally, it would be good if the bubble has a Vector3 value that it can call, but this at least centers everything and makes the bubbles more accessible.

--- GGJ-Sample/Assets/Scripts/CameraController.cs
+++ GGJ-Sample/Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,25 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float Speed = 5.0f;
+    public Vector3 startPos;
+
+    private void OnEnable(){
+        AppEvents.OnBubbleClick.OnTrigger += SnapToBubble;
+    }
+
+    private void OnDisable(){
+        AppEvents.OnBubbleClick.OnTrigger -= SnapToBubble;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (CreateBubblePanel.IsOpen) return;
-
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || (Input.mousePosition.y >= Screen.height && Input.mousePosition.y < Screen.height * 1.05))
         {
             transform.position += new Vector3(0, 1.0f, 0) * Speed * Time.deltaTime;
@@ -47,4 +55,9 @@ public class CameraController : MonoBehaviour
             Camera.main.orthographicSize += Speed * Time.deltaTime;
         }
     }
+
+    void SnapToBubble(Guid id){
+        this.transform.position = startPos;
+    }
 }
+
